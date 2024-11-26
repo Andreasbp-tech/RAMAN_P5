a210f9f7af357df340287ff012143894f888f63f
Mirenfeldt <rmejlvir@gmail.com>
2024-11-26 11:29:42 +0100

    Updateret standarder for værdierne i aktivitetsloggen

--- raman/lib/globals.dart
+++ raman/lib/globals.dart
@@ -2,17 +2,29 @@ library globals;
 
 int bottomNavigationBarIndex = 0;
 
-List<String> activities = [
-  'Exercise',
-  'Læs en bog',
-  'Meditation',
-  'Work',
-  'Træning',
-  'Cook a meal',
-  'Watch TV',
-  'Go for a walk',
-  'Spil et spil',
-  'Listen to music',
-  'Clean the house',
-  'Do laundry',
-];
+Map<String, bool> activities = {
+  'Træning': false,
+  'Læs en bog': false,
+  'Meditation': false,
+  'Arbejde': false,
+  'Lave mad': false,
+  'Se TV': false,
+  'Gå en tur': false,
+  'Spil et spil': false,
+  'Lytte til musik': false,
+  'gøre rent': false,
+  'vaske tøj': false,
+};
+Map<String, int> activitiesPrevalence = {
+  'Træning': 0,
+  'Læs en bog': 0,
+  'Meditation': 0,
+  'Arbejde': 0,
+  'Lave mad': 0,
+  'Se TV': 0,
+  'Gå en tur': 0,
+  'Spil et spil': 0,
+  'Lytte til musik': 0,
+  'gøre rent': 0,
+  'vaske tøj': 0,
+};
